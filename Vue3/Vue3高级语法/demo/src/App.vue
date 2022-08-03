df736f1053af61f714aa1a81f7f06bf86ba7ba40
地中海强者 <101050485+Overmatch-of-The-Mediterranean@users.noreply.github.com>
2022-08-03 13:53:58 +0800

    yahu~

---
+++ Vue3/Vue3高级语法/demo/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <input v-model="inputValue" type="text">
+    <button @click="handleAddItem">提交</button>
+    <ul>
+      <List-item v-for="(item,index) in list" :key="index" :msg="item" :index="index" :list="list"/>
+    </ul>
+  </div>
+</template>
+
+<script>
+import ListItem from './components/ListItem.vue';
+import {reactive,ref} from 'vue';
+export default {
+  name: 'App',
+  components: {
+    ListItem
+  },
+  setup(){
+    const inputValue = ref(123);
+    const list = reactive([]);
+    const handleAddItem = function(){
+      list.push(inputValue.value);
+      inputValue.value = '';
+    };
+    return { inputValue, list, handleAddItem }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ Vue3/Vue3高级语法/demo/src/components/ListItem.vue
@@ -0,0 +1,26 @@
+<template>
+    <li @click="handleDeleteItem(index,list)">{{ msg }}</li>
+</template>
+
+<script>
+export default {
+  name: 'ListItem',
+  props: {
+    msg: String,
+    index:Number,
+    list:Array
+  },
+  setup(){
+    const handleDeleteItem = (index,list)=>{
+      console.log(index,list);
+      list.splice(index,1);
+    }
+    return { handleDeleteItem };
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+
+</style>
